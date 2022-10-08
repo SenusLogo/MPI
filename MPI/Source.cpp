c49e1244199245fff28c876e3584e93443ba34b8
SenusLogo <Senus.Evov@yandex.ru>
2022-10-08 10:49:21 +0900

    for all

--- MPI/Source.cpp
+++ MPI/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Windows.h>
+#include <time.h>
 #include "mpi.h"
 
 using namespace std;
@@ -11,10 +12,17 @@ int main(int* argc, char** argv)
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	int thread, thread_size;
-	double* number;
+	//int* A;
+	//int* B;
+	//int* local_sum;
+	//int local_summ;
 
-	const char operation[4] = { '+', '-', '/', '*' };
+	//int size = 0;
+	//int summ = 0;
+
+	int thread, thread_size;
+	//double* number;
+	char hello[12] = "hello world";
 
 	MPI_Init(argc, &argv);
 
@@ -23,30 +31,104 @@ int main(int* argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &thread);
 	MPI_Comm_size(MPI_COMM_WORLD, &thread_size);
 
-	if (thread_size != 4)
-	{
-		cout << "Кол-во процессоров не верно" << endl;
-		return 1;
-	}
+	//number = new double[2]; // new memory for each processor
 
-	number = new double[2]; // new memory for each processor
+	//if (thread == ZERO_PROCCESSOR)
+	//{
+	//	cout << "Size array: ";
+	//	cin >> size;
+	//}
+
+	//MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	//A = new int[size];
+	//B = new int[size];
+
+	//if (thread == ZERO_PROCCESSOR)
+	//{
+	//	srand(unsigned int(time(0)));
+	//	for (int i(0); i < size; i++)
+	//		A[i] = rand() % 20;
+
+	//	for (int i(0); i < size; i++)
+	//	{
+	//		cout << A[i] << " ";
+	//	}
+	//	cout << endl;
+	//}
+
+	//MPI_Scatter(A, size / thread_size, MPI_INT, B, size / thread_size, MPI_INT, ZERO_PROCCESSOR, MPI_COMM_WORLD);
+
+	//local_sum = new int[thread_size];
+	//for (int i(0); i < thread_size; i++)
+	//	local_sum[i] = 0;
+
+	//local_summ = 0;
+	//for (int j(0); j < size / thread_size; j++)
+	//	local_summ += B[j];
+
+	//MPI_Gather(&local_summ, 1, MPI_INT, local_sum, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	//if (thread == ZERO_PROCCESSOR)
+	//{
+	//	int result1 = 0;
+	//	for (int i(0); i < size; i++)
+	//		result1 += A[i];
+	//	cout << result1 << endl;
+
+	//	int result = 0;
+	//	for (int i(0); i < thread_size; i++)
+	//		result += local_sum[i];
+	//	cout << result << endl;
+	//}
 
 	if (thread == ZERO_PROCCESSOR)
 	{
-		cout << "Enter number: ";
-		for (int i(0); i < 2; i++)
-			cin >> number[i]; // enter 2 number
+		//cout << "Enter number: ";
+		//for (int i(0); i < 2; i++)
+		//	cin >> number[i]; // enter 2 number
 
-		cout << "a = " << number[0] << "\nb = " << number[1] << endl;
+		//cout << "a = " << number[0] << "\nb = " << number[1] << endl;
 
-		for (int thread_lvl(1); thread_lvl < thread_size; thread_lvl++)
-			MPI_Send(number, 2, MPI_DOUBLE, thread_lvl, 0, MPI_COMM_WORLD);
+		cout << hello << " " << thread << endl;
+		MPI_Send(hello, 1, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
 
-		cout << "Send successful." << endl;
+		//cout << "Send successful." << endl;
 
-	} else MPI_Recv(number, 2, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+	}
+	else if (thread > ZERO_PROCCESSOR && thread < thread_size - 1)
+	{
+		MPI_Recv(&hello, 1, MPI_CHAR, thread - 1, 0, MPI_COMM_WORLD, &status);
 
-	cout << "a " << operation[thread] << " b = " << (thread == 0 ? number[0] + number[1] : thread == 1 ? number[0] - number[1] : thread == 2 ? number[0] / number[1] : number[0] * number[1]);
+		cout << hello << " " << thread << endl;
+
+		MPI_Send(&hello, 1, MPI_CHAR, thread + 1, 0, MPI_COMM_WORLD);
+	}
+	else
+	{
+		MPI_Recv(&hello, 1, MPI_CHAR, thread - 1, 0, MPI_COMM_WORLD, &status);
+
+		cout << hello << " " << thread << endl;
+	}
+
+	/*cout << "a " << operation[thread] << " b = " << (thread == 0 ? number[0] + number[1] : thread == 1 ? number[0] - number[1] : thread == 2 ? number[0] / number[1] : number[0] * number[1]);*/
+
+	/*switch (thread)
+	{
+	case 0:
+		cout << "a + b = " << number[0] + number[1] << endl;
+		break;
+	case 1:
+		cout << "a - b = " << number[0] - number[1] << endl;
+		break;
+	case 2:
+		cout << "a * b = " << number[0] * number[1] << endl;
+		break;
+	case 3:
+		cout << "a / b = " << number[0] / number[1] << endl;
+		break;
+
+	}*/
 
 	MPI_Finalize();
 
